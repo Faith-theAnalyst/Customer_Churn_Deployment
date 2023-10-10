977bac6c9c2e5b06fe1d5552392bf93af10bb115
Faith Esther Mwai <code.faith.17@gmail.com>
2023-10-10 20:19:39 +0300

    Modify files

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 import gradio as gr
 import pandas as pd
 import pickle
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
-def load_ml_components(fp):
-    "Load the ml component to reuse in App"
+
+def load_model(fp):
+    "Load the model to reuse in App"
     with open(fp, "rb") as f:
-        object = pickle.load(f)
-    return object
+        model = pickle.load(f)
+    return model
 
-model = load_ml_components("model.sav")
-pipeline = load_ml_components("pipeline.sav")
+best_model = load_model("Src/churn_model.pkl")
+
+# Creating the preprocessing pipeline
+categorical_features = ['gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService',
+                        'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+                        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'tenure_group']
+
+numerical_features = ['SeniorCitizen', 'MonthlyCharges', 'TotalCharges']
+
+categorical_transformer = OneHotEncoder(drop='first')
+numerical_transformer = StandardScaler()
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', categorical_transformer, categorical_features),
+        ('num', numerical_transformer, numerical_features)
+    ]
+)
 
 def predict(SeniorCitizen, MonthlyCharges, TotalCharges, gender, Partner, Dependents, PhoneService, 
             MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport,
@@ -24,28 +44,20 @@ def predict(SeniorCitizen, MonthlyCharges, TotalCharges, gender, Partner, Depend
                                          'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
                                          'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
                                          'PaymentMethod', 'tenure'])
-                                         
-    # Process the 'tenure' column
-    if new_df['tenure'].isnull().any():
-        new_df['tenure'].fillna(new_df['tenure'].median(), inplace=True)
-    new_df['tenure'] = new_df['tenure'].astype(int)
 
     labels = ["{0} - {1}".format(i, i + 11) for i in range(1, 72, 12)]
     new_df['tenure_group'] = pd.cut(new_df['tenure'], range(1, 80, 12), right=False, labels=labels)
     new_df.drop(columns=['tenure'], axis=1, inplace=True)
 
-    # Transforming the data using the pipeline
-    processed_data = pipeline.transform(new_df)
-    
-    single = model.predict(processed_data)
-    probablity = model.predict_proba(processed_data)[:,1]
-    
+    processed_data = preprocessor.transform(new_df)
+    single = best_model.predict(processed_data)
+    probablity = best_model.predict_proba(processed_data)[:,1]
+
     if single == 1:
         return "This customer is likely to be churned with a confidence of {:.2f}%".format(probablity*100)
     else:
         return "This customer is likely to continue with a confidence of {:.2f}%".format(probablity*100)
 
-# Define the Gradio interface
 iface = gr.Interface(
     fn=predict,
     inputs=[
@@ -67,10 +79,12 @@ iface = gr.Interface(
         gr.Dropdown(label='Contract', choices=["Month-to-month", "One year", "Two year"]),
         gr.Dropdown(label='PaperlessBilling', choices=["Yes", "No"]),
         gr.Dropdown(label='PaymentMethod', choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"]),
-        gr.Number(label='tenure: 0-72', min_value=0, max_value=72)
+        gr.Number(label='tenure: 0-72')
     ],
     outputs="text",
-    live=True
+    theme="dark",
+    title="Customer Churn Prediction",
+    description="Predict whether a customer is likely to churn."
 )
 
 iface.launch(share=True)
