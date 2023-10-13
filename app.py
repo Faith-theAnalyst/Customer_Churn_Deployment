6f2b1843bf13046c162e0ce543d7617e6cda7a89
Faith Esther Mwai <code.faith.17@gmail.com>
2023-10-13 15:21:40 +0300

    Modify files

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 # Load model
-with open('Src/rfc.pkl', 'rb') as file:
+with open('Src/churn_model.pkl', 'rb') as file:
     rfc_loaded = pickle.load(file)
 
 # Load preprocessor
-with open('Src/preprocessor.pkl', 'rb') as file:
+with open('Src/churn_pipeline.pkl', 'rb') as file:
     preprocessor_loaded = pickle.load(file)
 
 # Creating the preprocessing pipeline
@@ -56,46 +56,58 @@ def predict(SeniorCitizen, MonthlyCharges, TotalCharges, gender, Partner, Depend
     else:
         return "This customer is likely to continue."
 
-with gr.Blocks(title="Customer Churn Prediction", theme="monochrome") as iface:
+input_components = []
+with gr.Blocks(theme= "monochrome") as iface:
     
-    gr.Label("Customer Churn Prediction")
-    gr.Markdown("# Predict whether a customer is likely to churn.")
+    title="Customer Churn Prediction" 
+    
+    img = gr.Image("churn.jpg",height='700', width='500')
+     
+    with gr.Row():
+        title = gr.Label("Customer Churn Prediction")
+        
+    with gr.Row():
+        img
+        
+    with gr.Row():
+        gr.Markdown(" # Predict whether a customer is likely to churn.")
 
     with gr.Row():
-        gr.Dropdown(label='SeniorCitizen', choices=[0, 1]),
-        gr.Number(label='MonthlyCharges'),
-        gr.Number(label='TotalCharges'),
-        gr.Dropdown(label='gender', choices=["Male", "Female"]),
-        gr.Dropdown(label='Partner', choices=["Yes", "No"])
+        with gr.Column(scale=3, min_width=600):
+        
+            input_components=[
+                gr.Radio(label='Are you a Seniorcitizen; No=0 and Yes=1', choices=[0, 1]),
+                gr.Number(label='Enter Monthly Charges'),
+                gr.Number(label='Enter Total Charges'),
+                gr.Radio(label='Select your Gender', choices=["Male", "Female"]),
+                gr.Radio(label='Do you have a Partner', choices=["Yes", "No"]),
+                gr.Radio(label='Do you have any Dependents? ', choices=["Yes", "No"]),
+                gr.Radio(label='Do you have PhoneService?', choices=["Yes", "No"]),
+                gr.Dropdown(label='Do you have MultipleLines', choices=["Yes", "No", "No phone service"]),
+                gr.Dropdown(label='Do you have InternetService', choices=["DSL", "Fiber optic", "No"]),
+                gr.Dropdown(label='Do you have OnlineSecurity', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='Do you have OnlineBackup', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='Do you have DeviceProtection', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='Do you have TechSupport', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='Do you have StreamingTV', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='Do you have StreamingMovies', choices=["Yes", "No", "No internet service"]),
+                gr.Dropdown(label='What is your Contract with Telco', choices=["Month-to-month", "One year", "Two year"]),
+                gr.Radio(label='Do you prefer PaperlessBilling?', choices=["Yes", "No"]),
+                gr.Dropdown(label='Which PaymentMethod do you prefer?', choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"]),
+                gr.Slider(minimum=0, maximum=72, label='What is the length of Tenure with Telco : (0-72 months)')
+
+                ]
     
     with gr.Row():
-        gr.Dropdown(label='Dependents', choices=["Yes", "No"]),
-        gr.Dropdown(label='PhoneService', choices=["Yes", "No"]),
-        gr.Dropdown(label='MultipleLines', choices=["Yes", "No", "No phone service"]),
-        gr.Dropdown(label='InternetService', choices=["DSL", "Fiber optic", "No"]),
-        gr.Dropdown(label='OnlineSecurity', choices=["Yes", "No", "No internet service"])
-
-    with gr.Row():
-        gr.Dropdown(label='OnlineBackup', choices=["Yes", "No", "No internet service"]),
-        gr.Dropdown(label='DeviceProtection', choices=["Yes", "No", "No internet service"]),
-        gr.Dropdown(label='TechSupport', choices=["Yes", "No", "No internet service"]),
-        gr.Dropdown(label='StreamingTV', choices=["Yes", "No", "No internet service"]),
-        gr.Dropdown(label='StreamingMovies', choices=["Yes", "No", "No internet service"])
+        pred = gr.Button('Predict')
+        
     
-    with gr.Row():
-        gr.Dropdown(label='Contract', choices=["Month-to-month", "One year", "Two year"]),
-        gr.Dropdown(label='PaperlessBilling', choices=["Yes", "No"]),
-        gr.Dropdown(label='PaymentMethod', choices=["Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"]),
-        gr.Slider(minimum=0, maximum=72,  label='tenure: 0-72')
-
-    with gr.Row():
-        pred= gr.Button('Predict')
-
+    
     output_components = gr.Label(label='Churn') 
     
     pred.click(fn=predict,
-            #inputs=input_components,
+            inputs=input_components,
             outputs=output_components,
-            )
-
+            )        
+           
 iface.launch(share=True, debug=True)
